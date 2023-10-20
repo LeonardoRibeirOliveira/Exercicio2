bed7eafea2c8f4757ba33e433fe6694afbf2c907
LeonardoRibeirOliveira <leonardo25palmeira250@gmail.com>
2023-10-20 15:20:42 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Artigos.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio2.Models
+{
+    public class Artigos
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public ICollection<Data>? datas { get; set; } //Expressa a relação de 1 categoria para n Datas
+
+    }
+}
---
+++ Models/Data.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercicio2.Models
+{
+    public class Data
+    {
+        public string? title { get; set; }
+        public string url { get; set; }
+        public string author { get; set; }
+        public string? num_comments { get; set; }
+        public int? story_id { get; set; }
+        public string? story_title { get; set; }
+        public string? story_url { get; set; }
+        public DateTime created_at { get; set; }
+        public int pageArtigo { get; set; }
+        public Data? data { get; set; }  //Relaciona a data por propriedade de navegação ao artigo
+    }
+}
